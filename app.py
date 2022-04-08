9e7a381c83506589886e3a9f8db9ec2f313d9f5f
Gaurav Kumar <gauravk26800@gmail.com>
2022-04-08 13:35:52 +0530

    updated app to flask app

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
 # Import libraries
 import numpy as np
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, request, jsonify, render_template
 import pickle
 
+
 app = Flask(__name__)
-cors = CORS(app)
 
 # Load the model
 model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/api', methods=['POST'])
-@cross_origin()
-def predict():
-    # Get the data from the POST request.
-    data = request.get_json(force=True)
-
-    # Make prediction using model loaded from disk as per the data.
-    prediction = model.predict([np.array(data['exp'])])
-
-    # Take the first value of prediction
+def predict(height, radius, freq):
+    prediction = model.predict([np.array([height, radius, freq])])
     output = prediction[0]
+    return output
 
-    return jsonify(output)
+@app.route("/", methods=['GET','POST'])
+def homepage():
+    if(request.method=="POST"):
+        output = predict(request.form["height"], request.form["radius"], request.form["freq"])
+        return render_template("index.html", result=output)
 
+    return render_template("index.html")
+
+# @app.route('/api', methods=['POST'])
+# def predict():
+#     # Get the data from the POST request.
+#     data = request.get_json(force=True)
+
+#     # Make prediction using model loaded from disk as per the data.
+#     prediction = model.predict([np.array(data['exp'])])
+
+#     # Take the first value of prediction
+#     output = prediction[0]
+
+#     return jsonify(output)
+
+# @app.route('/sub', methods=['POST'])
+# def submit():
+#     if request.method == "POST":
+#         name=request.form["username"]
+
+#     return render_template("sub.html", n=name)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)

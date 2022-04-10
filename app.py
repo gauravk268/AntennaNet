37e23703dbdf3f05a235d8abbd2a986e99148f53
Gaurav Kumar <gauravk26800@gmail.com>
2022-04-10 23:42:29 +0530

    updated range prediction functionality

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 # Import libraries
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, make_response
 import pickle
+import pandas as pd
+
 
 app = Flask(__name__)
 
+
 # Load the model
 model = pickle.load(open('model.pkl', 'rb'))
+
 
 def predict(height, radius, freq):
     # Make prediction using model loaded from disk as per the data.
@@ -14,19 +18,21 @@ def predict(height, radius, freq):
 
     # Take the first value of prediction
     output = prediction[0]
-
     return output
+
 
 def predict_range(height, radius, freq):
     input_values = np.stack(np.meshgrid(height, radius, freq), -1).reshape(-1, 3)
-    return input_values
+    prediction = model.predict(input_values)
+    final_result = np.c_[input_values, prediction]
+    return final_result
 
 
 # for single value
-
 @app.route("/", methods=['GET','POST'])
 def homepage():
     if(request.method=="POST"):
+        print(request.form)
         output = predict(request.form["height"], request.form["radius"], request.form["freq"])
         return render_template("index.html", result=output)
 
@@ -34,18 +40,25 @@ def homepage():
 
 
 # for range values
-
 @app.route("/range", methods=['GET','POST'])
 def range_input():
     if(request.method=="POST"):
-        height = np.arange(request.form["height_start"], request.form["height_stop"]+request.form["height_step"], request.form["height_step"])
-        radius = np.arange(request.form["radius_start"], request.form["radius_stop"]+request.form["radius_step"], request.form["radius_step"])
-        freq = np.arange(request.form["freq_start"], request.form["freq_stop"]+request.form["freq_step"], request.form["freq_step"])
+        # generating input combination
+        height = np.arange(float(request.form["height_start"]), float(request.form["height_end"])+float(request.form["height_step"]), float(request.form["height_step"]))
+        radius = np.arange(float(request.form["radius_start"]), float(request.form["radius_end"])+float(request.form["radius_step"]), float(request.form["radius_step"]))
+        freq = np.arange(float(request.form["freq_start"]), float(request.form["freq_end"])+float(request.form["freq_step"]), float(request.form["freq_step"]))
 
-        predict_range(height, radius, freq)
-        return render_template("range_input.html", result=1.2)
+        # predicting result
+        output = predict_range(height, radius, freq)
+
+        # generating csv result
+        resp = make_response(pd.DataFrame(output, columns=["Height [mm]", "Radius [mm]", "Freq [GHz]", "S11 [dB]"]).to_csv(index=False))
+        resp.headers["Content-Disposition"] = "attachment; filename=range_output.csv"
+        resp.headers["Content-Type"] = "text/csv"
+        return resp
 
     return render_template("range_input.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)

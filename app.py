40cec3cfd5965e86333c488efc75e0a0ac79d4b2
Gaurav Kumar <gauravk26800@gmail.com>
2022-04-10 12:45:53 +0530

    added html file for range values

--- app.py
+++ app.py
@@ -3,15 +3,18 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-
 app = Flask(__name__)
 
 # Load the model
 model = pickle.load(open('model.pkl', 'rb'))
 
 def predict(height, radius, freq):
+    # Make prediction using model loaded from disk as per the data.
     prediction = model.predict([np.array([height, radius, freq])])
+
+    # Take the first value of prediction
     output = prediction[0]
+
     return output
 
 @app.route("/", methods=['GET','POST'])
@@ -22,25 +25,14 @@ def homepage():
 
     return render_template("index.html")
 
-# @app.route('/api', methods=['POST'])
-# def predict():
-#     # Get the data from the POST request.
-#     data = request.get_json(force=True)
+@app.route("/range", methods=['GET','POST'])
+def range_input():
+    if(request.method=="POST"):
+        console.log(request.form);
+        output = predict(request.form["height"], request.form["radius"], request.form["freq"])
+        return render_template("range_input.html", result=output)
 
-#     # Make prediction using model loaded from disk as per the data.
-#     prediction = model.predict([np.array(data['exp'])])
-
-#     # Take the first value of prediction
-#     output = prediction[0]
-
-#     return jsonify(output)
-
-# @app.route('/sub', methods=['POST'])
-# def submit():
-#     if request.method == "POST":
-#         name=request.form["username"]
-
-#     return render_template("sub.html", n=name)
+    return render_template("range_input.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
